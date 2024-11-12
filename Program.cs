03edc759291d0bc59971795e7b598415eea3e5ed
andreas24sfj <andreas24sfj@gmail.com>
2024-11-12 10:31:30 +0100

    setup user input parsing

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿// Les av bruker input
-Console.WriteLine("Write something interesting in the console:");
-string? userInput = Console.ReadLine();
+﻿
 
 
-// Ekko tilbake det bruker skrev inn
-Console.WriteLine("Echo: " + userInput);
+
+
+bool runProgram = true;
+while (runProgram)
+{
+    // Les av bruker input
+    Console.WriteLine("Do you want to lend or return?");
+    string? userInput = Console.ReadLine();
+
+    // Vi må finne ut hva bruker skrev inn
+    // For å låne en bok (lend)
+    if (userInput == "lend")
+    {
+        Console.WriteLine("Lending a book");
+    }
+
+    // For å levere tilbake en bok (return)
+    else if (userInput == "return"){
+        Console.WriteLine("Returning a book");
+    }
+
+    // For å avslutte (exit)
+    else if (userInput == "exit"){
+        runProgram = false;
+    }
+}
+
